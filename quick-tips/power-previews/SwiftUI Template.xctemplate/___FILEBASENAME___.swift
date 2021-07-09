3d2cf1d4a86134096825527610a8335940f8e01a
Adam Fordyce <adamspostbox-codeslice@yahoo.com>
2021-07-09 23:52:46 +0100

    Add template for power previews

---
+++ quick-tips/power-previews/SwiftUI Template.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,25 @@
+
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    var body: some View {
+        VStack {
+            /*@START_MENU_TOKEN@*/Text("Let's go!").font(.title)/*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    struct ___FILEBASENAMEASIDENTIFIER____Harness: View {
+
+        var body: some View {
+            ___FILEBASENAMEASIDENTIFIER___()
+        }
+    }
+
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER____Harness()
+            .previewDevice("iPhone 12 Pro Max")
+            .previewDisplayName("iPhone 12 Pro Max")
+    }
+}
